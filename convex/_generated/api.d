ecf5ee41ba2665cb22cfda9e70fa7a2775a4428d
ferrik <50550603+ferrik@users.noreply.github.com>
2025-11-29 18:50:41 +0200

    Create api.d

---
+++ convex/_generated/api.d
@@ -0,0 +1,65 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
+import type * as import_from_sheets from "../import_from_sheets.js";
+import type * as import_helpers from "../import_helpers.js";
+import type * as menu_items from "../menu_items.js";
+import type * as orders from "../orders.js";
+import type * as partners from "../partners.js";
+import type * as promocodes from "../promocodes.js";
+import type * as users from "../users.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  http: typeof http;
+  import_from_sheets: typeof import_from_sheets;
+  import_helpers: typeof import_helpers;
+  menu_items: typeof menu_items;
+  orders: typeof orders;
+  partners: typeof partners;
+  promocodes: typeof promocodes;
+  users: typeof users;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
